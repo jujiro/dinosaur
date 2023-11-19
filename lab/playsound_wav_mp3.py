e8f579ae56ecd27a828a7282e9a1e70102e78f33
Ashish Kumar <ash.c3po@gmail.com>
2023-11-19 18:02:56 -0500

    Renamed files

---
+++ lab/playsound_wav_mp3.py
@@ -0,0 +1,4 @@
+import playsound
+# Not tested with PI
+
+playsound.playsound('./audio/sample.mp3', True)
---
+++ lab/pygame_wav-mp3.py
@@ -0,0 +1,17 @@
+import pygame
+# Plays both MP3 and Wave files
+
+
+pygame.init()
+#snd=pygame.mixer.Sound('./audio/sample.wav')
+#pygame.mixer.music.load('./audio/sample.wav')
+pygame.mixer.music.load('./audio/sample.mp3')
+pygame.mixer.music.play()
+#snd.play()
+#pygame.time.wait(30000)
+while True:
+  if pygame.mixer.music.get_busy():
+    pygame.time.wait(1000)
+  else:
+    break
+pygame.quit()
